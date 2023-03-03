1dc6f7670a58d3121e3355a692059548d71f16a6
Vanjo Loiz <akgamishanks01@gmail.com>
2023-03-04 01:45:56 +0800

    Fix production base url

--- utils/baseUrl.ts
+++ utils/baseUrl.ts
@@ -1,1 +1,4 @@
-export const BASE_URL = 'http://localhost:3000';
+export const BASE_URL =
+  process.env.NODE_ENV === 'production'
+    ? 'https://salvadorloiz.onrender.com'
+    : 'http://localhost:3000';
