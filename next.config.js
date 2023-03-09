7cc497495f6743e95f0f64067e39c360fade179e
Vanjo Loiz <akgamishanks01@gmail.com>
2023-03-09 13:32:50 +0800

    Remove zustand

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  swcMinify: false,
+  swcMinify: true,
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
