5157a1f9d8b501c17723d1948e87187f6f1ee328
Vanjo Loiz <akagamishanks01@gmail.com>
2023-04-01 17:12:12 +0800

    Remove process exit 1

--- config/db.ts
+++ config/db.ts
@@ -1,31 +1,25 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-require('dotenv').config({ path: './config.env' });
+require("dotenv").config({ path: "./config.env" });
 
 const MONGO_URI = process.env.MONGO_URI?.replace(
-  '<PASSWORD>',
+  "<PASSWORD>",
   process.env.DB_PASSWORD!
 );
 
-mongoose.set('strictQuery', true);
+mongoose.set("strictQuery", true);
 
 const connectDb = async () => {
-  try {
-    const con = await mongoose.connect(MONGO_URI!, {
-      //@ts-ignore
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-      // useCreateIndex: true,
-      // useFindAndModify: false,
-    });
+  const con = await mongoose.connect(MONGO_URI!, {
+    //@ts-ignore
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    // useCreateIndex: true,
+    // useFindAndModify: false,
+  });
 
-    //@ts-ignore
-    console.log(`MongoDB Connected: ${con.connection.host}`);
-  } catch (error) {
-    //@ts-ignore
-    console.error(`Error: ${error.message}`);
-    process.exit(1);
-  }
+  //@ts-ignore
+  console.log(`MongoDB Connected: ${con.connection.host}`);
 };
 
 export default connectDb;
