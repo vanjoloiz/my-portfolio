5363fb8d0647203b66434defe63e477cfd625d34
Vanjo Loiz <akagamishanks01@gmail.com>
2023-04-01 09:02:55 +0800

    Disable swc minify

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  swcMinify: true,
+  // swcMinify: true,
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
       issuer: /\.[jt]sx?$/,
-      use: [{ loader: '@svgr/webpack', options: { ref: true } }],
+      use: [{ loader: "@svgr/webpack", options: { ref: true } }],
     });
 
     return config;
