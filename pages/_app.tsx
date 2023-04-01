bb4820bc5f4058c131e88255fcb85a58191c7647
Vanjo Loiz <akagamishanks01@gmail.com>
2023-04-01 10:11:37 +0800

    Fix favicon icon not displaying

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -59,7 +59,7 @@ export default function MyApp(props: MyAppProps) {
         <title>Salvador Loiz</title>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
         <meta name="Salvador Loiz portfolio website" content="Salvador Loiz" />
-        {/* <link rel="icon" href="/favicon.ico" /> */}
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <SWRConfig value={{ fetcher }}>
         <ColorModeContext.Provider value={colorMode}>
