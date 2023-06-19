e2f2eb6920f44493074d510672831d79fc8edda1
Vanjo Loiz <akagamishanks01@gmail.com>
2023-06-20 06:52:52 +0800

    Add the github repo link on footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,12 +1,26 @@
 import { FC } from "react";
 import Typography from "@mui/material/Typography";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import Box from "@mui/material/Box";
+import Link from "@mui/material/Link";
 
 const Footer: FC = () => {
   return (
-    <footer style={{ marginTop: "10px" }}>
+    <footer style={{ marginTop: "10px", marginBottom: "15px" }}>
       <Typography variant="subtitle1" align="center" gutterBottom>
         Copyright © Salvador Loiz. 2023.
       </Typography>
+
+      <Box sx={{ display: "flex", justifyContent: "center" }}>
+        <Link
+          href="https://github.com/vanjoloiz/my-portfolio"
+          color="inherit"
+          target="_blank"
+          rel="noopener"
+        >
+          <GitHubIcon sx={{ cursor: "pointer" }} />
+        </Link>
+      </Box>
     </footer>
   );
 };
