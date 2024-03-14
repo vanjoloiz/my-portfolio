cf2872665c87168abd0799363985c98867c63824
Vanjo Loiz <akagamishanks01@gmail.com>
2024-03-14 20:13:32 +0800

    Remove process sigint

--- server.ts
+++ server.ts
@@ -65,11 +65,6 @@ nextApp.prepare().then(async () => {
 
         io.emit("updateViewsCount", data.length);
       });
-
-      process.on("SIGINT", async () => {
-        await axios.delete(`${BASE_URL}/api/v1/count/${userId}`);
-        process.exit(0);
-      });
     });
   });
 
