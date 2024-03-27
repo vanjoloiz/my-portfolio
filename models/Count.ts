4e5998279edd580ba0af27424c3b48fb2d765411
Vanjo Loiz <akagamishanks01@gmail.com>
2024-03-27 15:55:13 +0800

    Add timestamps

--- models/Count.ts
+++ models/Count.ts
@@ -1,16 +1,19 @@
 import mongoose from "mongoose";
 
-const CountSchema = new mongoose.Schema({
-  userId: {
-    type: String,
-    required: true,
-  },
+const CountSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
 
-  ipAddress: {
-    type: String,
-    required: true,
+    ipAddress: {
+      type: String,
+      required: true,
+    },
   },
-});
+  { timestamps: true }
+);
 
 const CountModel = mongoose.model("Count", CountSchema);
 
