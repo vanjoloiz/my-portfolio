dbe959eabe61b36afd0c1f0988512f233cbab729
Vanjo Loiz <akagamishanks01@gmail.com>
2025-08-13 02:38:43 +0800

    Fix render error

--- next.config.js
+++ next.config.js
@@ -4,29 +4,15 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
 
-const isRender = process.env.RENDER === "true";
-
 module.exports = withBundleAnalyzer({
-  swcMinify: true,
+  swcMinify: false,
   reactStrictMode: false,
-  experimental: isRender
-    ? {
-        swcLoader: true,
-      }
-    : {},
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
       issuer: /\.[jt]sx?$/,
       use: [{ loader: "@svgr/webpack", options: { ref: true } }],
     });
-
-    if (isRender) {
-      config.experiments = {
-        ...config.experiments,
-        asyncWebAssembly: true,
-      };
-    }
 
     return config;
   },
