472d5894f67d6f1d5fe77ed4ef77e73b37f68ae2
Vanjo Loiz <akagamishanks01@gmail.com>
2025-08-13 02:31:34 +0800

    Enable swc again

--- next.config.js
+++ next.config.js
@@ -4,15 +4,29 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
 
+const isRender = process.env.RENDER === "true";
+
 module.exports = withBundleAnalyzer({
-  swcMinify: false,
+  swcMinify: true,
   reactStrictMode: false,
+  experimental: isRender
+    ? {
+        swcLoader: true,
+      }
+    : {},
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
       issuer: /\.[jt]sx?$/,
       use: [{ loader: "@svgr/webpack", options: { ref: true } }],
     });
+
+    if (isRender) {
+      config.experiments = {
+        ...config.experiments,
+        asyncWebAssembly: true,
+      };
+    }
 
     return config;
   },
