a2b279408f500d3e93ff39a8dd781c404c184f47
Vanjo Loiz <akagamishanks01@gmail.com>
2025-08-13 01:26:08 +0800

    Upgrade node version to 20

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
 });
 
 module.exports = withBundleAnalyzer({
+  swcMinify: false,
   reactStrictMode: false,
   webpack(config) {
     config.module.rules.push({
